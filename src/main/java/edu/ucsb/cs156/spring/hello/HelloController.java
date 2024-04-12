0cc818eb00fa52874694cdf855a040b18bf9430c
kylume <123840358+kylume@users.noreply.github.com>
2024-04-11 17:35:18 -0700

    change greeting message

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from ukyl!";
+        return "Greetings from kevin777!";
     }
     
 }
